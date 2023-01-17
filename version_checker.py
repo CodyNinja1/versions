11cbba678f9e0eaea7c776a0959efab6346ce6fa
Stuntlover <101748104+Stuntlover-TM@users.noreply.github.com>
2023-01-17 16:04:07 +0100

    Update version_checker.py

--- version_checker.py
+++ version_checker.py
@@ -2,7 +2,7 @@ import requests
 import ctypes
 
 def check_for_updates():
-    version_url = 'https://raw.githubusercontent.com/Stuntlover-TM/versions/main/example_vc_.txt' # This URL should stay the same in all versions of the program
+    version_url = 'https://raw.githubusercontent.com/Stuntlover-TM/versions/main/example_vc.txt' # This URL should stay the same in all versions of the program
     newest_release_url = requests.get(version_url).text.split("\n") # Splits the URL for each newline, newest_release_url[0] being the download URL and newest_release_url[1] being the version
     current_release_url = "https://github.com/Stuntlover-TM/versions/releases/download/example-v1.0.0/version-checker.exe" # The "example-v1.0.0" would change if you made a new release
 
